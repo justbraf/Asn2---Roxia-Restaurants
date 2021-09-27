ede9373cc77ae437dc9041e9b6cbd0f4c1a8cac5
justbraf <dwaynecorey+github@gmail.com>
2021-09-27 00:56:15 -0400

    Skeleton structure complete

---
+++ RTSDemo.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class RTSDemo {
+    public static void main(String[] args) {
+        Scanner inputStream = new Scanner(System.in);
+        System.out.println("Welcome to the Roxia Management System.");
+
+        inputStream.close();
+    }
+}
